8fe38c35bf1cd984cba49066b2892c0e8a92bc13
feverus <98666447+feverus@users.noreply.github.com>
2023-09-06 12:50:49 +0300

    Модуль Welcome

--- components/welcome/welcome.tsx
+++ components/welcome/welcome.tsx
@@ -3,15 +3,24 @@ import styles from './welcome.module.scss'
 
 export default function Welcome() {
   return (
-    <section>
-      обложка сайта
+    <section className={styles.welcome}>
+      <div className={styles.image}>
+      <Image
+        src="/images/welcomeImage.jpg"
+        fill
+        alt="Изображение нашего лучшего дома"
+      />
+      </div>
+      <div className={styles.slogan}>
+      Дом бизнес-класса<br/>для ценителей роскоши
+      </div>
+      <div className={styles.name}>
+      <Image
+        src="/svgs/inchapin-welcome.svg"
+        fill
+        alt="Изображение нашего лучшего дома"
+      />
+      </div>
     </section>
   )
 }
-/**
-<Image
-  src="/images/welcomeImage.jpg"
-  fill
-  alt="Picture of the author"
-/>
-*/
